2c13e3c34c12f9bba6a209dd8be6787e348f18b4
mbsproano-collab <mbs.proano@yavirac.edu.ec>
2026-06-17 22:53:56 -0500

    Update Ejercicio de Constructor.ts

--- Constructor/Ejercicio de Constructor.ts
+++ Constructor/Ejercicio de Constructor.ts
@@ -1,23 +1,17 @@
-// ejercicio con constructor.
-// Programacion Orientada a Objetos.
-
 class Persona {
     private nombre: string;
     private edad: number;
-
-    // Constructor
+    
     constructor(nombre: string, edad: number) {
         this.nombre = nombre;
         this.edad = edad;
     }
 
-    // Método para mostrar datos
     public mostrarDatos(): void {
         console.log(`Nombre: ${this.nombre}`);
         console.log(`Edad: ${this.edad}`);
     }
 }
 
-// "Main"
 const persona1 = new Persona("Juan", 20);
 persona1.mostrarDatos();
